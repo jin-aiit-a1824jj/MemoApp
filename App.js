48c85869015fdd4486a79ed7b27d39277b2563c1
a1824jj <a1824jj@aiit.ac.jp>
2020-03-17 16:05:40 +0900

    add firebase

--- App.js
+++ App.js
@@ -7,6 +7,22 @@ import LoginScreen from './src/screens/LoginScreen'
 import SignupScreen from './src/screens/SignupScreen'
 
 
+import ENV from './env.json';
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: ENV.FIREBASE_API_KEY,
+    authDomain: ENV.FIREBASE_AUTH_DOMAIN,
+    databaseURL: ENV.FIREBASE_DATABASE_URL,
+    projectId: ENV.FIREBASE_PROJECT_ID,
+    storageBucket: ENV.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: ENV.FIREBASE_MESSAGING_SENDER_ID,
+    appId: ENV.FIREBASE_APP_ID
+};
+
+firebase.initializeApp(firebaseConfig);
+
+
 const headerOption = { 
     headerTitle: 'Memot',
     headerStyle:{
@@ -15,14 +31,16 @@ const headerOption = {
     headerTitleStyle: {
       color: '#fff',
     },
+    headerTintColor: '#fff',
+    headerBackTitle: null,
   };
 
 const AppNavigator = createStackNavigator({
+  Login:     { screen: LoginScreen, navigationOptions: headerOption },
+  Signup:    { screen: SignupScreen, navigationOptions: headerOption },
   Home:      { screen: MemoListScreen, navigationOptions: headerOption },
   MemoDetail:{ screen: MemoDetailScreen, navigationOptions: headerOption },
   MemoEdit:  { screen: MemoEditScreen, navigationOptions: headerOption },
-  Login:     { screen: LoginScreen, navigationOptions: headerOption },
-  Signup:    { screen: SignupScreen, navigationOptions: headerOption }
 });
 
 
